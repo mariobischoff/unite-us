796bfc8b553be9bfcfd2807f9ae14d5d40c4545e
Mario Bischoff <mariobischoffneto@gmail.com>
2020-04-02 17:28:42 -0300

    Comecei a implementar o componente BaseHeader

---
+++ frontend/components/BaseHeader/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <div class="logo">
+        <img src="~/assets/logo.svg" alt="Logo Unite US">
+      </div>
+      <div class="links">
+        <nuxt-link
+          v-for="(path, index) in paths"
+          :key="index"
+          :to="path.path"
+        >
+          {{ path.name }}
+        </nuxt-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      paths: [
+        { name: 'Home', path: '/' },
+        { name: 'Equipes', path: '/' },
+        { name: 'Profissionais', path: '/' },
+        { name: 'Sobre', path: '/' },
+        { name: 'Login', path: '/' },
+        { name: 'Cadastrar', path: '/' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navbar {
+  background-color: #952597;
+  display: flex;
+  height: 120px;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.links > a {
+  text-decoration: none;
+  color: whitesmoke;
+  font-weight: bold;
+  font-size: 1.8em;
+  margin-right: 25px;
+}
+
+.logo > img {
+  margin-left: 25px;
+  width: 120px;
+}
+</style>
