67f9984df00bde0254d029cc837c0d6a92d3c6d1
ThazSobral <thazsobral@gmail.com>
2020-04-10 18:15:15 -0300

    reestruturaçao de pasta

---
+++ frontend/components/BaseHeader.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <div class="logo">
+        <img src="~/assets/logo-negative.svg" alt="Logo Unite US">
+      </div>
+      <div class="links">
+        <nuxt-link
+          v-for="(path, index) in paths"
+          :key="index"
+          exact-active-class="active-link"
+          :to="path.path"
+        >
+          {{ path.name }}
+        </nuxt-link>
+
+        <nuxt-link
+          v-for="authPath in auth"
+          :key="authPath.path"
+          exact-active-class="active-link"
+          :to="authPath.path"
+        >
+          {{ authPath.name }}
+        </nuxt-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      paths: [
+        { name: 'Home', path: '/' },
+        { name: 'Equipes', path: '/team' },
+        { name: 'Profissionais', path: '/professionals' },
+        { name: 'Sobre', path: '/about' }
+      ],
+      auth: [
+        { name: 'Login', path: '/signin' },
+        { name: 'Cadastrar-se', path: '/signup' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.navbar
+  background-color: $primary
+  height: 110px
+  box-shadow: 0px 1px 3px 0px rgba(0,0,0,0.75)
+  display: flex
+  flex-direction: row
+  align-items: center
+
+.navbar > .logo
+  flex: 1
+
+.navbar > .logo > img
+  margin-left: 30px
+  width: 100px
+
+.navbar > .links > a
+  flex: 3
+  color: $secondary
+  text-decoration: none
+  font-weight: bold
+  font-size: 1.2em
+  margin-right: 20px
+
+.active-link
+  color: $accent !important
+</style>
