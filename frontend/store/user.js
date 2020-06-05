be0eae0c667772d90843d60d511c32386d699d11
Mário Bischoff <mariobischoffneto@gmail.com>
2020-06-05 18:51:34 -0300

    Alterei store do user

--- frontend/store/user.js
+++ frontend/store/user.js
@@ -1,18 +1,14 @@
 export const state = () => ({
-  fullName: null,
-  auth: false,
-  error: null
+  user: null,
+  auth: false
 })
 
 export const mutations = {
   SET_AUTH (state, auth) {
     state.auth = auth
   },
-  SET_NAME (state, name) {
-    state.fullName = name
-  },
-  SET_ERROR (state, error) {
-    state.error = error
+  SET_USER (state, user) {
+    state.user = user
   }
 }
 
@@ -20,27 +16,22 @@ export const getters = {
   isAuth (state) {
     return state.auth
   },
-  fullName (state) {
-    return state.fullName
+  getUser (state) {
+    return state.user
   }
 }
 
 export const actions = {
   async login ({ commit }, payload) {
-    try {
-      commit('SET_ERROR', null)
-      const { name, token } = await this.$axios.$post('/users/auth', payload)
-      localStorage.setItem('token', token)
-      commit('SET_AUTH', true)
-      commit('SET_NAME', name)
-    } catch (error) {
-      commit('SET_ERROR', error.message)
-    }
+    const { user, token } = await this.$axios.$post('/users/auth', payload)
+    localStorage.setItem('token', token)
+    commit('SET_USER', user)
+    commit('SET_AUTH', true)
   },
   async register ({ commit }, payload) {
     try {
       commit('SET_ERROR', null)
-      await this.$axios.post('/user', payload)
+      await this.$axios.post('/users', payload)
     } catch (error) {
       commit('SET_ERROR', error.message)
     }
