75f8fdd35292682e97e9a5e04167d5f1231ae636
Mário Bischoff <mariobischoffneto@gmail.com>
2020-06-05 15:01:37 -0300

    Criei o store dos Teams

---
+++ frontend/store/team.js
@@ -0,0 +1,37 @@
+export const state = () => ({
+  teams: [],
+  error: null
+})
+
+export const mutations = {
+  SET_TEAM (state, teams) {
+    state.teams = teams
+  },
+  SET_ERROR (state, error) {
+    state.error = error
+  }
+}
+
+export const getters = {
+  getTeams (state) {
+    return state.teams
+  }
+}
+
+export const actions = {
+  async fetchTeams ({ commit }) {
+    try {
+      const teams = await this.$axios.$get('teams')
+      commit('SET_TEAM', teams)
+    } catch (error) {
+      commit('SET_ERROR', error.message)
+    }
+  },
+  async createTeam ({ commit }, payload) {
+    try {
+      await this.$axios.post('teams', payload)
+    } catch (error) {
+      commit('SET_ERROR', error.message)
+    }
+  }
+}
