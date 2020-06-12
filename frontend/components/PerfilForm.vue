678c809c1a1dcead1888f2eafcbe385d95d9244d
thazsobral <thazsobral@gmail.com>
2020-06-12 06:08:00 -0300

    estilizei o form de alteraçao de informaçoes do usuario

--- frontend/components/PerfilForm.vue
+++ frontend/components/PerfilForm.vue
@@ -1,72 +1,101 @@
 <template>
-  <v-row justify="center">
-    <v-dialog v-model="dialog" persistent max-width="600px">
-      <v-card>
-        <v-card-title>
-          <span class="headline">Alterar Perfil</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" md="6">
-                <v-text-field v-model="user.name" label="Nome Completo" />
-              </v-col>
-              <v-col cols="12" md="4">
-                <v-text-field v-model="user.email" label="Email" disabled />
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-text-field v-model="user.expertise" label="Especialidade" />
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-text-field v-model="user.lastGraduation" label="Ultima Formação Academica" />
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-text-field v-model="user.city" label="Cidade" />
-              </v-col>
-              <v-col cols="12" md="6">
-                <v-select
-                  v-model="user.workAt"
-                  label="Forma de trabalho"
-                  :items="workAtOptions"
-                />
-              </v-col>
-              <v-col cols="12" class="ma-0">
-                <v-combobox
-                  v-model="user.skills"
-                  label="Habilidades"
-                  chips
-                  counter="5"
-                  append-icon
-                  multiple
-                />
-              </v-col>
-              <v-col cols="12">
-                <v-range-slider
-                  v-model="user.pretension"
-                  label="Pretenção Salarial"
-                  max="50000"
-                  min="1000"
-                  step="1000"
-                  thumb-label
-                  thumb-color="green"
-                  thumb-size="38"
-                />
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="red darken-1" text @click="$emit('handleForm')">
-            Voltar
-          </v-btn>
-          <v-btn color="green darken-1" text @click="submit">
-            Salvar
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-row>
+  <v-dialog
+    v-model="dialog"
+    persistent
+    max-width="600px"
+    scrollable
+  >
+    <v-card>
+      <v-card-title>
+        <p class="content-title">
+          Alterar Perfil
+        </p>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="6">
+              <v-text-field v-model="user.name" label="Nome Completo" color="#952597" />
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-text-field v-model="user.email" label="Email" disabled color="#952597" />
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-select
+                v-model="user.expertise"
+                label="Especialidade"
+                color="#952597"
+                :items="expertiseOptions"
+                item-color="purple"
+                attach
+                chip
+              />
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.lastGraduation" label="Ultima Formação Academica" color="#952597" />
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="user.city" label="Cidade" color="#952597" />
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-select
+                v-model="user.workAt"
+                label="Forma de trabalho"
+                :items="workAtOptions"
+                color="#952597"
+                item-color="purple"
+              />
+            </v-col>
+            <v-col cols="12" class="ma-0">
+              <v-select
+                v-model="user.skills"
+                :items="skillsOptions"
+                attach
+                label="Habilidades"
+                chips
+                counter="5"
+                append-icon
+                multiple
+                color="#952597"
+                item-color="purple"
+              />
+            </v-col>
+            <v-col cols="12">
+              <v-range-slider
+                v-model="user.pretension"
+                label="Pretenção Salarial"
+                max="50000"
+                min="1000"
+                step="1000"
+                thumb-label="always"
+                thumb-color="#952597"
+                color="#952597"
+                track-color="#cdd1ce"
+                thumb-size="38"
+              />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <button
+          class="button-any"
+          text
+          @click="$emit('handleForm')"
+        >
+          Cancelar
+        </button>
+        <v-spacer />
+        <button
+          class="button-accept"
+          depressed
+          @click="submit"
+        >
+          Salvar
+        </button>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -95,6 +124,20 @@ export default {
   },
   data () {
     return {
+      expertiseOptions: [
+        'Design',
+        'Developer',
+        'RH'
+      ],
+      skillsOptions: [
+        'HTML',
+        'Python',
+        'Testando',
+        'Outra Skill Foda',
+        'Outra',
+        'Javascript',
+        'Ruby'
+      ],
       workAtOptions: [
         {
           text: 'Remoto',
