4712f3583193b773d1c78b5c216a2b67adfcf41e
Mário Bischoff <mariobischoffneto@gmail.com>
2020-06-15 17:22:54 -0300

    criei o service de payment

--- backend/src/routes/payment.js
+++ backend/src/routes/payment.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
+const auth = require('../middlewares/auth')
 const PaymentController = require('../controllers/payment')
-const Payment = require('../models/payment')
+const PaymentService = require('../services/payment')
 const User = require('../models/user')
 
-const paymentController = new PaymentController(Payment, User)
+const paymentController = new PaymentController(User, PaymentService)
 
-router.post('/', (req, res) => paymentController.create(req, res))
+router.post('/', auth, (req, res) => paymentController.create(req, res))
 
 module.exports = router
---
+++ backend/src/services/payment.js
@@ -0,0 +1,23 @@
+const pagarme = require('pagarme')
+
+module.exports = class PaymentService {
+  constructor (apiKey) {
+    this.apiKey = apiKey
+    this.planId = '486375'
+  }
+
+  async init (card) {
+    this.client = await pagarme.client.connect({ api_key: this.apiKey })
+    this.cardHash = await this.client.security.encrypt({ ...card })
+  }
+
+  async createSubscription (userEmail) {
+    this.client.subscriptions.create({
+      plan_id: this.planId,
+      card_hash: this.cardHash,
+      customer: {
+        email: userEmail
+      }
+    })
+  }
+}
